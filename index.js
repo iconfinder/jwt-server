afffb094fe704516919540c4642a076e56248a01
Emil Nikov <contacts@enstyled.com>
2020-05-12 16:38:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+var PORT = process.env.PORT;
 var CLIENT_ID = process.env.CLIENT_ID;
 var CLIENT_SECRET = process.env.CLIENT_SECRET;
 var AUTH_URL = "https://www.iconfinder.com/api/v3/oauth2/token";
 
 app.use(express.static('../public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     request.post(AUTH_URL, {
         form: {
             grant_type:'jwt_bearer',
             client_id: CLIENT_ID,
             client_secret: CLIENT_SECRET
         }
-    }, function (error, response, body) {
+    }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body);
             delete body.token_type;
             res.send(body);
         } else {
@@ -26,6 +26,6 @@ app.get('/', function (req, res) {
     });
 });
 
-app.listen(process.env.PORT, function () {
-    console.log('Example app listening on port 3334!');
+app.listen(PORT, function() {
+    console.log('Example app listening on port', PORT);
 });
